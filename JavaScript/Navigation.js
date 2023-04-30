04b0ab155c495294ed536fd319628c40a364297c
MrGamz <123112118+MrGamzWasTaken@users.noreply.github.com>
2023-04-29 20:01:30 -0400

    Saving

--- JavaScript/Navigation.js
+++
@@ -1,7 +0,0 @@
-// Get the navbar element
-const navbar = document.getElementById('navbar');
-
-// Function to toggle the width of the navbar
-function toggleNavbarWidth() {
-  navbar.classList.toggle('width-toggle');
-}
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Project Gateway
+# Welcome To Project Gateway!
 
-This Is Going To Be A Refurbished Version Of My Google Site
+##
